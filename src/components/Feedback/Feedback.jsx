af853d185435dafa232fc0599f8106f4e80eca57
NadiiaUa23 <hasiminka92@gmail.com>
2024-07-30 19:48:17 +0200

    add Math.round((good / totalFeedback) * 100) calculation of the percentage of positive reviews

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -3,12 +3,17 @@
 
 const Feedback = ({feedback}) => {
     const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+const  positiveFeedback = totalFeedback ? 
+Math.round((feedback.good / totalFeedback) * 100) : 0;
+
 
 return totalFeedback > 0 ? (
     <div>
-        <p>good:  {feedback.good}</p>
-        <p>neutral:  {feedback.neutral}</p>
-        <p>bad:  {feedback.bad}</p>
+        <p>Good:  {feedback.good}</p>
+        <p>Neutral:  {feedback.neutral}</p>
+        <p>Bad:  {feedback.bad}</p>
+        <p>Total: {totalFeedback}</p>
+        <p>Positive:  {positiveFeedback} %</p>
     </div>
 ) : (<p>No feedback yet</p>)
 
