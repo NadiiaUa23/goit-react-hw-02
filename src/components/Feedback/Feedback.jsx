4e2f30ba5904bd852cfa49732ada7611df815e46
NadiiaUa23 <hasiminka92@gmail.com>
2024-07-30 19:34:45 +0200

    Notification of statistics

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,18 +2,15 @@
 // import style from './Feedback.module.css'
 
 const Feedback = ({feedback}) => {
+    const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
 
-
-
-
-return (
+return totalFeedback > 0 ? (
     <div>
         <p>good:  {feedback.good}</p>
         <p>neutral:  {feedback.neutral}</p>
         <p>bad:  {feedback.bad}</p>
-        
     </div>
-)
+) : (<p>No feedback yet</p>)
 
 
 };
