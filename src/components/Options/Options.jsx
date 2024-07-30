ae943e112c08e66d4927dfba61ee726a359f3e12
NadiiaUa23 <hasiminka92@gmail.com>
2024-07-30 18:39:15 +0200

    creating feedback buttons

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,15 +1,27 @@
 import style from './Options.module.css'
  
 const Options = ({feedback, setFeedback}) => {
-    
+    const updateFeedback = feedbackType => {
+        setFeedback(prevFeedback => ({
+            ...prevFeedback, [feedbackType]:
+            prevFeedback[feedbackType] + 1
+         
+        }));
+        };
+        
+        const resetFeedback = () => {
+            setFeedback({good: 0,
+                neutral: 0,
+                bad: 0});
+        };
 
 
     return (
 <div>
-    <button onClick={() => {}}>good</button>
-    <button onClick={() => {}}>neutral</button>
-    <button onClick={() => {}}>bad</button>
-    {/* <button onClick={() =>{} }>Reset</button> */}
+    <button onClick={()=>updateFeedback('good')}>good</button>
+    <button onClick={()=>updateFeedback('neutral')}>neutral</button>
+    <button onClick={()=>updateFeedback('bad')}>bad</button>
+    <button onClick={()=>resetFeedback}>Reset</button>
 </div>
     );
 };
