b98bd81a9ea762723d416a7db58a7fdf964a3e72
NadiiaUa23 <hasiminka92@gmail.com>
2024-08-03 14:56:35 +0200

    add discription. foo add in the App/.button reset fix.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect} from "react"
 import Feedback from './components/Feedback/Feedback'
 import Options from './components/Options/Options'
+import Description from './components/Description/Description'
 import  style from './App.module.css';
 
 const App = () => {
-  const [feedback, setFeedback] = 
-  useState ( () => {
+  const [feedback, setFeedback] = useState ( () => {
     const seveFeedback = localStorage.getItem('feedback');
     return seveFeedback ? JSON.parse(seveFeedback) : {
 	good: 0,
@@ -18,15 +18,23 @@ useEffect(() => {
     window.localStorage.setItem("feedback", JSON.stringify(feedback));
 }, [feedback]);
 
+const updateFeedback = (feedbackType) => {
+  setFeedback(prevFeedback => ({
+    ...prevFeedback,
+    [feedbackType]: prevFeedback[feedbackType] + 1
+  }));
+};
+
+const resetFeedback = () => {
+  setFeedback({ good: 0, neutral: 0, bad: 0 });
+};
+
+const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   return (
     <div className={style.App}>
-  <h1>Sip Happens Café
-  </h1>
-  <p>Please leave your feedback about our service by selecting one of the options below.
-  </p>
-
-<Options feedback={feedback} 
-setFeedback={setFeedback}/>
+<Description />
+<Options updateFeedback={updateFeedback} resetFeedback={resetFeedback} totalFeedback={totalFeedback} />
 <Feedback feedback={feedback}/>
     </div>
   );
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,37 +1,28 @@
 import style from './Options.module.css'
 import { FaThumbsUp, FaMeh, FaThumbsDown, FaRedo } from 'react-icons/fa';
 
-const Options = ({feedback, setFeedback}) => {
-    const updateFeedback = feedbackType => {
-        setFeedback(prevFeedback => ({
-            ...prevFeedback, 
-            [feedbackType]: prevFeedback[feedbackType] + 1
-        }));
-        };
-        
-        const resetFeedback = () => {
-            setFeedback({good: 0,
-                neutral: 0,
-                bad: 0});
-        };
-
+const Options = ({ updateFeedback, resetFeedback, totalFeedback })  => {
     return (
 <div className={style.options}>
-    <button onClick={()=>updateFeedback('good')}>
+    <button onClick={() => updateFeedback('good')}>
     <FaThumbsUp />
     <span>Good</span> 
     </button>
-    <button onClick={()=>updateFeedback('neutral')}>
+    <button onClick={() => updateFeedback('neutral')}>
     <FaMeh />
     <span>Neutral</span>
     </button>
-    <button onClick={()=>updateFeedback('bad')}>
+    <button onClick={() => updateFeedback('bad')}>
     <FaThumbsDown />
     <span>Bad</span>
     </button>
-    <button onClick={resetFeedback}> 
-        <FaRedo />
-    <span>Reset</span></button>
+    
+    {totalFeedback > 0 && (
+        <button onClick={resetFeedback}>
+          <FaRedo />
+          <span>Reset</span>
+        </button>
+      )}
 </div>
     );
 };
